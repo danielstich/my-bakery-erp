cc0542540015bcdc01d4e96af004fa58ef7a9260
danielstich <43919186+danielstich@users.noreply.github.com>
2022-01-19 18:45:43 -0500

    added mixins for text and flex centering

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -2,8 +2,11 @@ import './Home.scss'
 
 export default function Home() {
   return (
-    <div>
-        <h1>Home</h1>
+    <div className='Home'>
+        <h1 className='Home__Title'>Home</h1>
+        <h2 className='Home__Subtitle'>Subtitle</h2>
+        <h3 className='Home__Label'>Label</h3>
+        <p className='Home__Text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem atque quisquam sit? Soluta et beatae iste itaque facere odio possimus, voluptatum molestias porro non corporis aut aliquam consectetur, sit, eligendi similique nisi magni iusto repellendus eius. Praesentium iure suscipit reiciendis dolorum delectus sunt enim quisquam atque? Delectus quos autem, itaque sapiente libero magni amet. Dolor neque sit omnis et fugit tempora, quisquam unde corporis dolores, similique veniam iste a cum ratione aliquam ullam dolore vel totam quis non aut reprehenderit. Expedita, quo nesciunt consequuntur perferendis eligendi nihil eos sint perspiciatis quidem quod corrupti accusamus minus ipsa unde. Facilis, quasi deleniti.</p>
     </div>
   )
 }
