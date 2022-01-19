7808e648b0f7ec9d0d68295c99cedb1ca4338962
danielstich <43919186+danielstich@users.noreply.github.com>
2022-01-19 18:59:57 -0500

    added colors

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -7,6 +7,22 @@ export default function Home() {
         <h2 className='Home__Subtitle'>Subtitle</h2>
         <h3 className='Home__Label'>Label</h3>
         <p className='Home__Text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem atque quisquam sit? Soluta et beatae iste itaque facere odio possimus, voluptatum molestias porro non corporis aut aliquam consectetur, sit, eligendi similique nisi magni iusto repellendus eius. Praesentium iure suscipit reiciendis dolorum delectus sunt enim quisquam atque? Delectus quos autem, itaque sapiente libero magni amet. Dolor neque sit omnis et fugit tempora, quisquam unde corporis dolores, similique veniam iste a cum ratione aliquam ullam dolore vel totam quis non aut reprehenderit. Expedita, quo nesciunt consequuntur perferendis eligendi nihil eos sint perspiciatis quidem quod corrupti accusamus minus ipsa unde. Facilis, quasi deleniti.</p>
+
+        <div className="Home__Box Home__Box--1">
+          <p className="Home__Text Home__Text--Light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum recusandae alias repellendus dignissimos exercitationem et repellat aperiam perferendis aliquam doloremque, magnam labore animi autem dicta nulla impedit porro accusantium similique fugiat nisi maxime quidem quibusdam voluptatem! Nemo dolores incidunt eveniet, facilis minima omnis quod molestias nesciunt dolor, aut possimus!</p>
+        </div>
+
+        <div className="Home__Box Home__Box--2">
+        <p className="Home__Text Home__Text--Light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum recusandae alias repellendus dignissimos exercitationem et repellat aperiam perferendis aliquam doloremque, magnam labore animi autem dicta nulla impedit porro accusantium similique fugiat nisi maxime quidem quibusdam voluptatem! Nemo dolores incidunt eveniet, facilis minima omnis quod molestias nesciunt dolor, aut possimus!</p>
+        </div>
+
+        <div className="Home__Box Home__Box--3">
+        <p className="Home__Text Home__Text--Light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum recusandae alias repellendus dignissimos exercitationem et repellat aperiam perferendis aliquam doloremque, magnam labore animi autem dicta nulla impedit porro accusantium similique fugiat nisi maxime quidem quibusdam voluptatem! Nemo dolores incidunt eveniet, facilis minima omnis quod molestias nesciunt dolor, aut possimus!</p>
+        </div>
+
+        <div className="Home__Box Home__Box--4">
+        <p className="Home__Text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eum recusandae alias repellendus dignissimos exercitationem et repellat aperiam perferendis aliquam doloremque, magnam labore animi autem dicta nulla impedit porro accusantium similique fugiat nisi maxime quidem quibusdam voluptatem! Nemo dolores incidunt eveniet, facilis minima omnis quod molestias nesciunt dolor, aut possimus!</p>
+        </div>
     </div>
   )
 }
