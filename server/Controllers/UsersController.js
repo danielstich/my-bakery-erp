0e90e09f0933f6a58cd32c73b7d6573886034c9e
danielstich <43919186+danielstich@users.noreply.github.com>
2022-01-19 22:41:02 -0500

    added backend routes for signup and login

--- server/Controllers/UsersController.js
+++ server/Controllers/UsersController.js
@@ -1,5 +1,38 @@
-const Users = []
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-exports.signup = (req, res) => {
-    
+require('dotenv').config();
+
+const users = {}
+
+exports.signup = async (req, res) => {
+    const {name, email, password} = req.body;
+    if (!email || !password) return res.status(400).json({error: 'Email or Password is missing'});
+    if (users[email]) return res.status(400).json({error: 'User already Exists'});
+
+    const hash = await bcrypt.hash(password, 12);
+
+    const newUser ={
+        name,
+        email,
+        hash
+    }
+    users[email] = newUser;
+
+    res.status(201).send({success: 'User was created'})
+}
+
+exports.login = async (req, res) => {
+    const {email, password} = req.body;
+    if (!email || !password) return res.status(400).json({error: 'Email or Password is missing'});
+    if (!users[email]) return res.status(400).json({error: 'User not found'})
+    const hash = users[email].hash;
+    const passwordIsValid = await bcrypt.compare(password, hash);
+
+    if (!passwordIsValid) return res.status(401).json({error: 'Password is invalid'});
+
+    const user = users[email];
+    const token = jwt.sign(user, process.env.JWT_SECRET);
+
+    res.status(200).json({success: 'Login Succesful', token: token})
 }
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
     res.send('Server is Live')
 })
 
-app.use('/Users', UsersRouter)
+app.use('/users', UsersRouter)
 
 
 
