4922edcd24104359408224ef1a0d0ded351093f3
danielstich <43919186+danielstich@users.noreply.github.com>
2022-01-19 21:31:09 -0500

    added input field component

---
+++ client/src/Components/Input/InputField.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './InputField.scss'
+
+export default function InputField({ name, label, type, extraClasses, value, placeholder, onChangeHandler }) {
+    return (
+        <div className='Input'>
+            <label className='Input__Label' htmlFor={name}>{label}</label>
+            <input 
+                className={'Input__Field' + extraClasses}
+                type={type}
+                name={name}
+                id={name}
+                value={value}
+                placeholder={placeholder}
+                onChange={onChangeHandler}
+            />
+        </div>
+    );
+}
