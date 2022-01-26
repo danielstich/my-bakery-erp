4e20dbb29694bd6fe1f001292e6134c4c912a9e4
danielstich <43919186+danielstich@users.noreply.github.com>
2022-01-25 22:00:44 -0500

    renders list of recipes, finished styling

--- client/src/Components/Recipe/Recipes.jsx
+++ client/src/Components/Recipe/Recipes.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import './Recipes.scss';
 import editIcon from '../../Assets/Icons/edit.svg';
 import addIcon from '../../Assets/Icons/add.svg';
@@ -51,7 +51,10 @@ export default class Recipes extends Component {
 
         ]
 
-        this.setState({recipes: recipes});
+        this.setState({
+            recipes: recipes,
+            isLoading: false
+        });
     }
 
     selectRecipe = (recipe) => {
@@ -104,6 +107,19 @@ export default class Recipes extends Component {
                 <div className='Recipes__Header'>
                     <h1 className='Recipes__Title'>Recipes</h1>
                     <img onClick={this.addModal} className='Recipes__Add-Icon' src={addIcon} alt="add button" />
+                </div>
+                <div className='Recipes__List'>
+                    {!this.state.isLoading && this.state.recipes.map(recipe => {
+                        return (
+                            <div className='Recipe' key={recipe.id} onClick={() => this.selectRecipe(recipe)}>
+                                <div className='Recipe__Label-Container'>
+                                    <h3 className='Recipe__Title'>{recipe.name}</h3>
+                                    <p className='Recipe__Body'>{recipe.description}</p>
+                                </div>
+                                <img onClick={this.editModal} className='Recipe__Icon' src={editIcon} alt="" />
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         )
