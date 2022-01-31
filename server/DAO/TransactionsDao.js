fa21dc2b823e280f9484d6e267d1fc6960a9aa90
danielstich <43919186+danielstich@users.noreply.github.com>
2022-01-30 19:49:53 -0500

    Finished CRUD operations of TransactionsDAO.js

--- server/DAO/TransactionsDao.js
+++ server/DAO/TransactionsDao.js
@@ -1,7 +1,6 @@
 const knex = require('knex')(require('../knexfile').development);
 
 // create transaction
-
 exports.createTransaction = (transaciton, ktrx) => {
     if (!transaciton.description || !transaciton.date || !transaciton.user_id) {
         return new Error('Missing Transaction Fields')
@@ -12,10 +11,26 @@ exports.createTransaction = (transaciton, ktrx) => {
 }
 
 // read transactions
-
-// read single transaction
-
-// update transaction
+exports.readTransactions = (user_id, ktrx) => {
+    const promise = knex('transactions').where({user_id}).del();
+    if (ktrx) return promise.transacting(ktrx);
+    return promise;
+}
 
 // delete transaction
+exports.deleteTransaction = (user_id, id, ktrx) => {
+    const promise = knex('transactions').where({id, user_id}).del();
+    if (ktrx) return promise.transacting(ktrx);
+    return promise;
+}
 
+// update transaction
+exports.updateTransaction = (transaciton, ktrx) => {
+    const {id, user_id} = transaciton;
+    if (!transaciton.description || !transaciton.date || !transaciton.user_id) {
+        return new Error('Missing Transaction Fields')
+    }
+    const promise = knex('transactions').where({id, user_id}).update(transaciton)
+    if (ktrx) return promise.transacting(ktrx)           
+    return promise;
+}
